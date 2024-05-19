c14c4d0f99b1f555cd51c2db2afca9112b46792b
GustavoMoraes21 <gustavo.dmoraes@sptech.school>
2024-05-19 00:38:23 -0300

    Acerto de CSS de acordo com resolução de tela

--- Site/src/database/script-tabelas.sql
+++ Site/src/database/script-tabelas.sql
@@ -1,14 +1,6 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+CREATE DATABASE carmeeting;
 
-/*
-comandos para mysql server
-*/
-
-CREATE DATABASE aquatech;
-
-USE aquatech;
+USE carmeeting;
 
 CREATE TABLE empresa (
 	id INT PRIMARY KEY AUTO_INCREMENT,
